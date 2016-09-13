71eac66263146b356adb2a1e033d1a9c05387a1a
EzequielCaballero <ezequiel.caballero33@gmail.com>
2016-09-12 22:46:36 -0300

    Inicio routeo ng & ui-router

--- js/miAplicacion.js
+++ js/miAplicacion.js
@@ -1,5 +1,39 @@
 /*MI APLICACION ANGULAR JS*/
-var miApp = angular.module('CursoIngresoJS', []);
+var miApp = angular.module('CursoIngresoJS', ['ui.router']);
+
+miApp.config(function($stateProvider, $urlRouterProvider){
+	$stateProvider
+	.state(
+    	"MenuPrincipal", {
+    		url:"/MenuPrincipal",
+    		templateUrl:"Partes/MenuPrincipal/menuPrincipal.html",
+    		controller:"controlMenuPrincipal"
+
+    	}
+    	)
+    .state(
+    	"login", {
+    		url:"/login",
+    		views:{
+    			"contenido":{
+    				templateUrl:"Partes/Login/login.html",
+    				controller:"controlLogin"
+    			}
+    		}
+
+    	}
+    	)
+
+    $urlRouterProvider.otherwise("/MenuPrincipal");
+});
+
+miApp.controller('controlMenuPrincipal', function($scope, $state){
+	$scope.irLogin=function(){
+
+		$state.go("login");
+
+	};
+});
 
 miApp.controller('controlLogin', function($scope){
 
